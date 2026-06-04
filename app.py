1f272a2b10572481ef29c719508e59f5344122ac
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 12:59:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Recipe Intelligence",
+    layout="wide"
+)
+
+st.title("🍽 Recipe Intelligence Platform")
+
+st.markdown("""
+### Welcome
+
+Analyze millions of recipes using
+AI, NLP and Data Analytics.
+
+Use the sidebar to navigate.
+""")
